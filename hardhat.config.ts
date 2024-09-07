bba51bfe03d652303ffc2def461c1b8bedd6fe38
TenTimothy <saba@wmooragency.co>
2024-09-08 00:02:57 +0200

    deleted HelloWorld and Lock

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,13 @@
 import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-/*const ETHERSCAN_API_KEY = vars.get('ETHERSCANAPI_KEY');
+const ETHERSCAN_API_KEY = vars.get('ETHERSCANAPI_KEY');
 const ALCHEMY_API_KEY = vars.get('ALCHEMY_API_KEY');
-const SEPOLIA_API_KEY = vars.get('SEPOLIA_API_KEY');*/
+const SEPOLIA_API_KEY = vars.get('SEPOLIA_API_KEY');
 
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
-  /*etherscan: {
+  etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
   networks: {
@@ -15,7 +15,7 @@ const config: HardhatUserConfig = {
       url: ``,
       accounts: [SEPOLIA_API_KEY],
     },
-  },*/
+  },
 };
 
 export default config;
--- ignition/modules/HelloWorld.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const HelloWorldModule = buildModule("HelloWorldModule", (m) => {
-    const helloWorld = m.contract("HelloWorld", [], {});  
-
-    return { helloWorld };
-});
-
-export default HelloWorldModule;
