17939377a956fac030d0c548c1f4228bbd63b114
TenTimothy <saba@wmooragency.co>
2024-09-08 02:42:12 +0200

    deployed to local host

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,6 @@ const ETHERSCAN_API_KEY = vars.get('ETHERSCANAPI_KEY');
 const ALCHEMY_API_KEY = vars.get('ALCHEMY_API_KEY');
 const SEPOLIA_API_KEY = vars.get('SEPOLIA_API_KEY');
 
-
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   etherscan: {
@@ -12,8 +11,11 @@ const config: HardhatUserConfig = {
   },
   networks: {
     sepolia: {
-      url: ``,
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [SEPOLIA_API_KEY],
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545", 
     },
   },
 };
